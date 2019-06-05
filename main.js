2e549d7b94e7ddb22e9da6de4a89bf885819a33d
AverkinaNastya <50995302+AverkinaNastya@users.noreply.github.com>
2019-06-05 17:36:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ var keyblack_img = new Image();
 keyblack_img.src = 'img/keyblack.png';
 var keygreen_img = new Image();
 keygreen_img.src = 'img/keygreen.png';
-var spike_img = new Image();
-spike_img.src = 'img/spikes.png';
 var lvls = [{
 	i1 : 0,
 	z1 : 0,
@@ -19,76 +17,122 @@ var lvls = [{
 	z2 : 6,
 	s : [[4,0,0,1,0,2,1],
 		 [1,1,0,1,0,1,1],
-		 [0,0,0,0,0,1,6],
+		 [0,0,0,0,0,1,0],
 		 [1,1,1,1,1,1,0],
 		 [3,1,0,0,0,0,0],
 		 [0,0,0,0,0,1,0],
 		 [1,1,1,1,5,1,0]],
-	doorblack : true,
-	doorgreen : true,
-	keygreen : true,
-	keyblack : true,
 }, {
 	i1 : 0,
 	z1 : 0,
 	i2 : 6,
 	z2 : 0,
 	s : [[0,0,0,1,0,0,0],
-		 [0,1,0,1,0,1,2],
-		 [4,1,0,0,0,1,1],
-		 [0,1,0,1,1,1,5],
+		 [4,1,0,1,0,1,2],
+		 [0,1,0,0,0,1,1],
+		 [0,6,0,1,1,6,5],
 		 [0,0,0,0,0,0,0],
 		 [1,0,1,1,1,1,0],
 		 [0,0,0,0,1,3,0]],
-	doorblack : true,
-	doorgreen : true,
-	keygreen : true,
-	keyblack : true,
+}, {
+	i1 : 0,
+	z1 : 2,
+	i2 : 5,
+	z2 : 5,
+	s : [[6,0,0,0,3,0,4],
+		 [0,0,1,1,1,0,1],
+		 [0,1,1,6,1,0,1],
+		 [0,0,0,0,0,6,1],
+		 [1,1,0,1,0,0,5],
+		 [2,0,1,0,0,0,6],
+		 [0,0,0,0,1,6,1]],
+}, {
+	i1 : 1,
+	z1 : 2,
+	i2 : 1,
+	z2 : 4,
+	s : [[1,1,6,0,0,6,1],
+		 [6,0,0,0,0,0,6],
+		 [0,0,1,6,0,0,3],
+		 [5,0,1,0,0,1,1],
+		 [0,0,0,0,0,0,0],
+		 [1,1,6,0,1,6,0],
+		 [2,0,0,0,1,4,0]],
+}, {
+	i1 : 2,
+	z1 : 2,
+	i2 : 2,
+	z2 : 3,
+	s : [[1,6,1,1,6,0,2],
+		 [5,0,6,6,0,0,6],
+		 [0,0,0,0,0,0,0],
+		 [6,0,1,6,0,1,0],
+		 [1,0,1,3,6,0,0],
+		 [6,0,1,0,1,0,6],
+		 [0,0,0,0,1,4,6]],
 }];
 var nom_lvl = 0;
 var motion = 0;
 var pos = 0;
-var spike_x;
-var spike_y;
+var i1 = lvls[0].i1;
+var i2 = lvls[0].i2;
+var z1 = lvls[0].z1;
+var z2 = lvls[0].z2;
+var s = lvls[0].s;
+var doorblack = true;
+var doorgreen = true;
+var keygreen = true;
+var keyblack = true;
+var keyblack_x;
+var keyblack_y;
+var keygreen_x;
+var keygreen_y;
+var doorblack_x;
+var doorblack_y;
+var doorgreen_x;
+var doorgreen_y;
+var x1 = z1 * 100;
+var y1 = i1 * 100;
+var x2 = z2*100;
+var y2 = i2*100;
+var mx1 = x1;
+var mx2 = x2;
+var my1 = y1;
+var my2 = y2;
 var canvas = document.getElementById('c1');
-canvas.width = lvls[nom_lvl].s[0].length * 100;
-canvas.height = lvls[nom_lvl].s.length * 100;
+canvas.width = s[0].length * 100;
+canvas.height = s.length * 100;
 var ctx = canvas.getContext('2d');
-draw_(lvls[nom_lvl].s,lvls[nom_lvl].keyblack_x,lvls[nom_lvl].keyblack_y,lvls[nom_lvl].keygreen_x,lvls[nom_lvl].keygreen_y, spike_x, spike_x);
+draw_(s,keyblack_x,keyblack_y,keygreen_x,keygreen_y);
+
 
 function draw_() {
-	for (var y = 0; y < lvls[nom_lvl].s.length; y++) {
-		for (var t = 0; t < lvls[nom_lvl].s[y].length; t++) {
-			if (lvls[nom_lvl].s[y][t] == 1){
+	for (var y = 0; y < s.length; y++) {
+		for (var t = 0; t < s[y].length; t++) {
+			if (s[y][t] == 1){
 				square_(y,t);
 			}
-			if (lvls[nom_lvl].s[y][t] == 2) {
-				lvls[nom_lvl].keyblack_x = t*100;
-				lvls[nom_lvl].keyblack_y = y*100;
+			if (s[y][t] == 2) {
+				keyblack_x = t*100;
+				keyblack_y = y*100;
 			}
-			if (lvls[nom_lvl].s[y][t] == 3) {
-				lvls[nom_lvl].keygreen_x = t*100;
-				lvls[nom_lvl].keygreen_y = y*100;
+			if (s[y][t] == 3) {
+				keygreen_x = t*100;
+				keygreen_y = y*100;
 			}
-			if (lvls[nom_lvl].s[y][t] == 4) {
-				lvls[nom_lvl].doorblack_x = t * 100;
-				lvls[nom_lvl].doorblack_y = y * 100;
+			if (s[y][t] == 4) {
+				doorblack_x = t * 100;
+				doorblack_y = y * 100;
 			}
-			if (lvls[nom_lvl].s[y][t] == 5) {
-				lvls[nom_lvl].doorgreen_x = t * 100;
-				lvls[nom_lvl].doorgreen_y = y * 100;
+			if (s[y][t] == 5) {
+				doorgreen_x = t * 100;
+				doorgreen_y = y * 100;
 			}
-			if (lvls[nom_lvl].s[y][t] == 6) {
-				spike_x = t * 100;
-				spike_y = y * 100;
-				// ctx.drawImage(spike_img,t * 100,y * 100);
+			if (s[y][t] == 6) {
+				spikes_(y,t);
 			}
 		}
 	}
-	lvls[nom_lvl].x1 = lvls[nom_lvl].z1 * 100;
-	lvls[nom_lvl].y1 = lvls[nom_lvl].i1 * 100;
-	lvls[nom_lvl].x2 = lvls[nom_lvl].z2*100;
-	lvls[nom_lvl].y2 = lvls[nom_lvl].i2*100;
 }
 function square_(y,t){
 	ctx.fillStyle = 'brown';
@@ -104,6 +148,22 @@ function square_(y,t){
 	ctx.fillRect(t*100+80,y*100+33,1,33);
 }
 
+function spikes_(y,t) {
+	ctx.fillStyle = 'grey';
+	ctx.beginPath();
+	for (var ys = 0; ys < 100; ys+=25) {
+		for (var xs = 0; xs < 100; xs+=25) {
+			ctx.moveTo(t*100+xs,y*100+25+ys);
+			ctx.lineTo(t*100+12.5+xs,y*100+ys);
+			ctx.lineTo(t*100+25+xs,y*100+25+ys);
+			ctx.lineTo(t*100+xs,y*100+25+ys);
+			ctx.fill();
+			ctx.stroke();
+			
+		}
+	}
+}
+
 function sh(keyCode) {
 	if ((keyCode*1 == 97)||(keyCode*1 == 1092)||(keyCode*1 == 100)||(keyCode*1 == 1074)){
 		left_right(keyCode);
@@ -114,125 +174,160 @@ function sh(keyCode) {
 	function left_right() {
 		if ((keyCode*1 == 97)||(keyCode*1 == 1092)) {
 			motion = -100;
-			lvls[nom_lvl].z1--;
-			lvls[nom_lvl].z2--;
+			z1--;
+			z2--;
 			pos = '+';
 			girlblack.src = 'img/girlblack4.png';
 			girlgreen.src = 'img/girlgreen4.png';
 		} else {
 			motion = 100;
-			lvls[nom_lvl].z1++;
-			lvls[nom_lvl].z2++;
+			z1++;
+			z2++;
 			pos = '-';
 			girlblack.src = 'img/girlblack2.png';
 			girlgreen.src = 'img/girlgreen2.png';
 		};
-		if ((lvls[nom_lvl].z1 < lvls[nom_lvl].s[lvls[nom_lvl].i1].length)&&(lvls[nom_lvl].z1>=0)&&(lvls[nom_lvl].s[lvls[nom_lvl].i1][lvls[nom_lvl].z1] != 1)) {
-			lvls[nom_lvl].my1 = lvls[nom_lvl].y1;
-			lvls[nom_lvl].mx1 = lvls[nom_lvl].x1;
-			lvls[nom_lvl].x1 += motion;
+		if ((z1 < s[i1].length)&&(z1>=0)&&(s[i1][z1] != 1)) {
+			my1 = y1;
+			mx1 = x1;
+			x1 += motion;
 		} else	if(pos == '+'){
-					lvls[nom_lvl].z1++;
-			    }else {lvls[nom_lvl].z1--}
-		if ((lvls[nom_lvl].z2 < lvls[nom_lvl].s[lvls[nom_lvl].i1].length)&&(lvls[nom_lvl].z2>=0)&&(lvls[nom_lvl].s[lvls[nom_lvl].i2][lvls[nom_lvl].z2] != 1)) {
-			lvls[nom_lvl].my2 = lvls[nom_lvl].y2;
-			lvls[nom_lvl].mx2 = lvls[nom_lvl].x2;
-			lvls[nom_lvl].x2 += motion;
+					z1++;
+			    }else {z1--}
+		if ((z2 < s[i1].length)&&(z2>=0)&&(s[i2][z2] != 1)) {
+			my2 = y2;
+			mx2 = x2;
+			x2 += motion;
 		} else	if(pos == '+'){
-					lvls[nom_lvl].z2++;
-			    }else {lvls[nom_lvl].z2--}
+					z2++;
+			    }else {z2--}
 
 	}
 	function top_bottom() {
 		if ((keyCode*1 == 119)||(keyCode*1 == 1094)) {
 			motion = -100;
-			lvls[nom_lvl].i1--;
-			lvls[nom_lvl].i2--;
+			i1--;
+			i2--;
 			pos = '+';
 			girlblack.src = 'img/girlblack3.png';
 			girlgreen.src = 'img/girlgreen3.png';
 		} else {
 			motion = 100;
-			lvls[nom_lvl].i1++;
-			lvls[nom_lvl].i2++;
+			i1++;
+			i2++;
 			pos = '-';
 			girlblack.src = 'img/girlblack1.png';
 			girlgreen.src = 'img/girlgreen1.png';
 		};
-		if ((lvls[nom_lvl].i1 < lvls[nom_lvl].s.length)&&(lvls[nom_lvl].i1>=0)&&(lvls[nom_lvl].s[lvls[nom_lvl].i1][lvls[nom_lvl].z1] != 1)) {
-			lvls[nom_lvl].mx1 = lvls[nom_lvl].x1;
-			lvls[nom_lvl].my1 = lvls[nom_lvl].y1;
-			lvls[nom_lvl].y1 += motion;
+		if ((i1 < s.length)&&(i1>=0)&&(s[i1][z1] != 1)) {
+			mx1 = x1;
+			my1 = y1;
+			y1 += motion;
 		} else	if(pos == '+'){
-					lvls[nom_lvl].i1++;
-			    }else {lvls[nom_lvl].i1--}
-		if ((lvls[nom_lvl].i2 < lvls[nom_lvl].s.length)&&(lvls[nom_lvl].i2>=0)&&(lvls[nom_lvl].s[lvls[nom_lvl].i2][lvls[nom_lvl].z2] != 1)) {
-			lvls[nom_lvl].mx2 = lvls[nom_lvl].x2;
-			lvls[nom_lvl].my2 = lvls[nom_lvl].y2;
-			lvls[nom_lvl].y2 += motion;
+					i1++;
+			    }else {i1--}
+		if ((i2 < s.length)&&(i2>=0)&&(s[i2][z2] != 1)) {
+			mx2 = x2;
+			my2 = y2;
+			y2 += motion;
 		} else	if(pos == '+'){
-					lvls[nom_lvl].i2++;
-			    }else {lvls[nom_lvl].i2--}
+					i2++;
+			    }else {i2--}
 	}
 }
-setInterval(function(){
-	if (lvls[nom_lvl].s[lvls[nom_lvl].i1][lvls[nom_lvl].z1] == 2) {
-		lvls[nom_lvl].keyblack = false;
-	}
-	if (lvls[nom_lvl].keyblack == true) {
-		ctx.clearRect(lvls[nom_lvl].keyblack_x,lvls[nom_lvl].keyblack_y,100,100);
-		ctx.drawImage(keyblack_img,lvls[nom_lvl].keyblack_x + 6,lvls[nom_lvl].keyblack_y);
-	}
-	if (lvls[nom_lvl].s[lvls[nom_lvl].i2][lvls[nom_lvl].z2] == 3) {
-		lvls[nom_lvl].keygreen = false;
-	}
-	if (lvls[nom_lvl].keygreen == true) {
-		ctx.clearRect(lvls[nom_lvl].keygreen_x,lvls[nom_lvl].keygreen_y,100,100);
-		ctx.drawImage(keygreen_img,lvls[nom_lvl].keygreen_x + 6,lvls[nom_lvl].keygreen_y);
-	}
-	if (lvls[nom_lvl].keyblack == false) {
-		lvls[nom_lvl].doorblack = false;
-	}
-	ctx.clearRect(lvls[nom_lvl].doorblack_x,lvls[nom_lvl].doorblack_y,100,100);
-	ctx.drawImage(doorblack_img,lvls[nom_lvl].doorblack_x + 10,lvls[nom_lvl].doorblack_y);
-	ctx.clearRect(lvls[nom_lvl].doorgreen_x,lvls[nom_lvl].doorgreen_y,100,100);
-	ctx.drawImage(doorgreen_img,lvls[nom_lvl].doorgreen_x + 10,lvls[nom_lvl].doorgreen_y);
-	if (lvls[nom_lvl].keygreen == false) {
-		lvls[nom_lvl].doorgreen = false;
-	}
-	
-	if ((lvls[nom_lvl].doorgreen == false)&&(lvls[nom_lvl].doorblack == false)&&(lvls[nom_lvl].x1 == lvls[nom_lvl].doorblack_x)&&(lvls[nom_lvl].x2 == lvls[nom_lvl].doorgreen_x)&&(lvls[nom_lvl].y1 == lvls[nom_lvl].doorblack_y)&&(lvls[nom_lvl].y2 == lvls[nom_lvl].doorgreen_y)) {
-		if (nom_lvl + 1 != lvls.length) {
-			nom_lvl++;
-				lvls[nom_lvl].x1 = lvls[nom_lvl].z1 * 100;
-				lvls[nom_lvl].y1 = lvls[nom_lvl].i1 * 100;
-				lvls[nom_lvl].x2 = lvls[nom_lvl].z2*100;
-				lvls[nom_lvl].y2 = lvls[nom_lvl].i2*100;
-				motion = 0;
-				pos = 0;
-				ctx.clearRect(0,0,canvas.width,canvas.height);
-				draw_(lvls);
-		}
-	}
-	if ((lvls[nom_lvl].x1 == lvls[nom_lvl].x2)&&(lvls[nom_lvl].y1 == lvls[nom_lvl].y2)){
-			lvls[nom_lvl].x1 = lvls[nom_lvl].z1 * 100;
-			lvls[nom_lvl].y1 = lvls[nom_lvl].i1 * 100;
-			lvls[nom_lvl].x2 = lvls[nom_lvl].z2*100;
-			lvls[nom_lvl].y2 = lvls[nom_lvl].i2*100;
+
+function repeat_(n) {
+			nom_lvl = n;
+			i1 = lvls[nom_lvl].i1;
+			i2 = lvls[nom_lvl].i2;
+			z1 = lvls[nom_lvl].z1;
+			z2 = lvls[nom_lvl].z2;
+			s = lvls[nom_lvl].s;
+			doorblack = true;
+			doorgreen = true;
+			keygreen = true;
+			keyblack = true;
+			x1 = z1 * 100;
+			y1 = i1 * 100;
+			x2 = z2*100;
+			y2 = i2*100;
+			mx1 = x1;
+			mx2 = x2;
+			my1 = y1;
+			my2 = y2;
+			girlblack.src = 'img/girlblack1.png';
+			girlgreen.src = 'img/girlgreen1.png';
+
 			motion = 0;
 			pos = 0;
 			ctx.clearRect(0,0,canvas.width,canvas.height);
-			draw_(lvls);
+			draw_(s);
+}
+
+setInterval(function(){
+	if (s[i1][z1] == 2) {
+		keyblack = false;
+	}
+	if (keyblack == true) {
+		ctx.clearRect(keyblack_x,keyblack_y,100,100);
+		ctx.drawImage(keyblack_img,keyblack_x + 6,keyblack_y);
+	}
+	if (s[i2][z2] == 3) {
+		keygreen = false;
+	}
+	if (keygreen == true) {
+		ctx.clearRect(keygreen_x,keygreen_y,100,100);
+		ctx.drawImage(keygreen_img,keygreen_x + 6,keygreen_y);
+	}
+	if (keyblack == false) {
+		doorblack = false;
+	}
+	ctx.clearRect(doorblack_x,doorblack_y,100,100);
+	ctx.drawImage(doorblack_img,doorblack_x + 10,doorblack_y);
+	ctx.clearRect(doorgreen_x,doorgreen_y,100,100);
+	ctx.drawImage(doorgreen_img,doorgreen_x + 10,doorgreen_y);
+	if (keygreen == false) {
+		doorgreen = false;
+	}
+	if ((doorgreen == false)&&(doorblack == false)&&(x1 == doorblack_x)&&(x2 == doorgreen_x)&&(y1 == doorblack_y)&&(y2 == doorgreen_y)) {
+		if (nom_lvl + 1 != lvls.length) {
+			nom_lvl++;
+
+			i1 = lvls[nom_lvl].i1;
+			i2 = lvls[nom_lvl].i2;
+			z1 = lvls[nom_lvl].z1;
+			z2 = lvls[nom_lvl].z2;
+			s = lvls[nom_lvl].s;
+			doorblack = true;
+			doorgreen = true;
+			keygreen = true;
+			keyblack = true;
+			x1 = z1 * 100;
+			y1 = i1 * 100;
+			x2 = z2*100;
+			y2 = i2*100;
+			mx1 = x1;
+			mx2 = x2;
+			my1 = y1;
+			my2 = y2;
+			motion = 0;
+			pos = 0;
+			ctx.clearRect(0,0,canvas.width,canvas.height);
+			draw_(s);
+		}
+	}
+	if ((s[i1][z1] == 6)||(s[i2][z2] == 6)||((x1 == x2)&&(y1 == y2))) {
+		repeat_(nom_lvl);
 	}
 
-	ctx.clearRect(lvls[nom_lvl].mx1, lvls[nom_lvl].my1, 100, 100);
-	ctx.clearRect(lvls[nom_lvl].mx2, lvls[nom_lvl].my2, 100, 100);
-	lvls[nom_lvl].mx1 = lvls[nom_lvl].x1;
-	lvls[nom_lvl].my1 = lvls[nom_lvl].y1;
-	lvls[nom_lvl].my2 = lvls[nom_lvl].y2;
-	lvls[nom_lvl].mx2 = lvls[nom_lvl].x2;
-	ctx.clearRect(lvls[nom_lvl].x1, lvls[nom_lvl].y1, 100, 100);
-	ctx.clearRect(lvls[nom_lvl].x2, lvls[nom_lvl].y2, 100, 100);
-	ctx.drawImage(girlblack,lvls[nom_lvl].x1,lvls[nom_lvl].y1);
-	ctx.drawImage(girlgreen,lvls[nom_lvl].x2,lvls[nom_lvl].y2);
+	ctx.clearRect(mx1, my1, 100, 100);
+	ctx.clearRect(mx2, my2, 100, 100);
+	mx1 = x1;
+	my1 = y1;
+	my2 = y2;
+	mx2 = x2;
+	ctx.clearRect(x1, y1, 100, 100);
+	ctx.clearRect(x2, y2, 100, 100);
+	ctx.drawImage(girlblack,x1,y1);
+	ctx.drawImage(girlgreen,x2,y2);
 },20);
